2c9aa6a163b04c9bd6354087e98411ce7b9e03e2
SethBastianUade <marroyooblitas@uade.edu.ar>
2026-09-04 18:53:52 -0300

    Ajustes

--- src/main/java/com/vapor/vapor/controller/compraController.java
+++ src/main/java/com/vapor/vapor/controller/compraController.java
@@ -1,4 +1,23 @@
 package com.vapor.vapor.controller;
 
-public class compraController {
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compras")
+public class CompraController {
+
+    @GetMapping("/mock")
+    public Map<String, Object> obtenerCompraMock() {
+        return Map.of(
+                "compra_id", "VAP-998877",
+                "estado", "aprobada",
+                "total", 120.50,
+                "moneda", "USD"
+        );
+    }
+
 }
