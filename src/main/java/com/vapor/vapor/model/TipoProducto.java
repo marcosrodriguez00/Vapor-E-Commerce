3aca17717ddc93a284860b79d4599eecfc7e83e9
Guillermo <kolpanino@gmail.com>
2026-09-18 03:59:03 -0300

    Agregue mas tipos de productos

--- src/main/java/com/vapor/vapor/model/TipoProducto.java
+++ src/main/java/com/vapor/vapor/model/TipoProducto.java
@@ -2,6 +2,10 @@ package com.vapor.vapor.model;
 
 /** Qué se está vendiendo: el juego base o contenido adicional. */
 public enum TipoProducto {
-    JUEGO,
-    DLC
+    // Guille: Agregue mas tipos de producto
+    BASE,
+    DLC,
+    EXPANSION_PACK,
+    EDICION_ESPECIAL
+
 }
